b3ab020d2949b761dc7149a1fbf64936451e8a53
Umberto Amenta <umberto.amenta@hotmail.it>
2025-03-12 23:11:51 +0100

    feat: add filtering functions in HomePage for newest and best-selling products

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,26 @@
-import axios from "../api/axios";
-import { useState, useEffect } from "react";
-
 // COMPONENTS
 import Hero from "../components/Hero";
 import FilteredSection from "../components/FilteredSection";
 
-// Placeholder
-import ProductCard from "../components/ProductCard";
-
 export default function HomePage() {
-  // filtro: i 10 prodotti più recenti (id più alto)
-  const filterNewProducts = (products) => {
+  // filter: 10 newest products (by id)
+  const filterNewestProducts = (products) => {
     return products.sort((a, b) => b.id - a.id).slice(0, 10);
   };
 
-  // filtro: i 10 prodotti più venduti (al momento i più costosi)
-  const filterTopSellingProducts = (products) => {
-    return products.sort((a, b) => b.price - a.price).slice(0, 10);
+  // filter: 10 best sellers
+  const filterBestSellersProducts = (products) => {
+    return products.sort((a, b) => b.sales - a.sales).slice(0, 10);
   };
+
   return (
     // container
     <div>
       <Hero />
-      <FilteredSection filterLogic={filterNewProducts}>
+      <FilteredSection filterLogic={filterNewestProducts}>
         <h3>Gli ultimi arrivi</h3>
       </FilteredSection>
-      <FilteredSection filterLogic={filterTopSellingProducts}>
+      <FilteredSection filterLogic={filterBestSellersProducts}>
         <h3>I più venduti</h3>
       </FilteredSection>
     </div>
