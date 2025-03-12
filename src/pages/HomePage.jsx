994617aee3dd0f9d01b7a897a637b8b27fb44571
Umberto Amenta <umberto.amenta@hotmail.it>
2025-03-12 19:44:36 +0100

    feat: enhance FilteredSection to accept filter logic; add filtering functions for new products in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,14 +9,23 @@ import FilteredSection from "../components/FilteredSection";
 import ProductCard from "../components/ProductCard";
 
 export default function HomePage() {
+  // filtro: i 10 prodotti più recenti (id più alto)
+  const filterNewProducts = (products) => {
+    return products.sort((a, b) => b.id - a.id).slice(0, 10);
+  };
+
+  // filtro: i 10 prodotti più venduti (al momento i più costosi)
+  const filterTopSellingProducts = (products) => {
+    return products.sort((a, b) => b.price - a.price).slice(0, 10);
+  };
   return (
     // container
     <div>
       <Hero />
-      <FilteredSection>
+      <FilteredSection filterLogic={filterNewProducts}>
         <h3>Gli ultimi arrivi</h3>
       </FilteredSection>
-      <FilteredSection>
+      <FilteredSection filterLogic={filterTopSellingProducts}>
         <h3>I più venduti</h3>
       </FilteredSection>
     </div>
