ee29dd696bed00f954a4ff904d59bf20462499ef
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-03-13 17:33:48 +0100

    feat: add URL image

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -14,7 +14,7 @@ export default function ProductCard({
       <div className="flex flex-row gap-5 bg-white rounded-lg shadow-md p-4 my-4 w-100 h-60 mx-auto  border-gray-200 transform transition duration-300 hover:scale-105">
         <div className="">
           <img
-            src={image}
+            src={`http://localhost:3000/imgs/${image}`}
             alt={name}
             className="w-30 h-35 object-cover rounded-lg"
           />
