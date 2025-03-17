7cc0623593a48c8996016677f7855a6170ab062b
Umberto Amenta <umberto.amenta@hotmail.it>
2025-03-17 14:47:49 +0100

    feat: add sorting functionality - SearchResultsPage

--- src/pages/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
 import { useSearchContext } from "../context/SearchContext";
 import ProductCard from "../components/ProductCard";
 
 export default function SearchResultsPage() {
   const { products } = useSearchContext(); // Ottiene i prodotti filtrati dal contesto di ricerca
+  const [sortOption, setSortOption] = useState("recent"); // Variabile per la gestione dell'opzione di ordinamento
+
+  // Ordinamento dei prodotti (.localeCompare distribuisce più stringhe in ordine alfabetico  )
+  const sortedProducts = [...products].sort((a, b) => {
+    if (sortOption === "price") {
+      return a.product_price - b.product_price;
+    } else if (sortOption === "name") {
+      return a.product_name.localeCompare(b.product_name);
+    } else {
+      return b.product_id - a.product_id;
+    }
+  });
 
   return (
-    <div>
-      {products.length > 0 ? ( // Se ci sono risultati, mostra la lista dei prodotti
-        <div className="container xl:max-w-320 mx-auto px-4">
+    <div className="container xl:max-w-320 mx-auto px-4">
+      <div className="py-4 text-xl">
+        <label className="font-semibold">Ordina per:</label>
+        <select
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+        >
+          <option value="recent">Recenti</option>
+          <option value="price">Prezzo</option>
+          <option value="name">Nome</option>
+        </select>
+      </div>
+      {sortedProducts.length > 0 ? ( // Se ci sono risultati, mostra la lista dei prodotti
+        <div>
           <div className="rounded-lg mx-auto">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-              {products.map((product) => (
+              {sortedProducts.map((product) => (
                 <div className="col" key={product.product_id}>
                   <ProductCard
                     name={product.product_name}
