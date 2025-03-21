da7544465986ec5ecb4cb80d8d06b8d6ffe61c2e
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-03-21 10:53:36 +0100

    fix cart and wishlist page style

--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useWishlist } from "../context/wishlistContext";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const WishlistPage = () => {
   const { wishlist, removeFromWishlist } = useWishlist();
+  const { slug } = useParams();
 
   return (
-    <div className="max-w-6xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">La tua Wishlist</h1>
-      <div>
+    <div className="max-w-6xl mt-12 mx-auto p-4">
+      <div className="">
         {wishlist.length === 0 ? (
           <div className="flex flex-col items-center justify-center">
             <Link to="/search">
@@ -25,8 +26,11 @@ const WishlistPage = () => {
         ) : (
           <ul>
             {wishlist.map((item) => (
-              <li key={item.id} className="flex justify-between mb-4">
-                <Link to={`/product/${item.product_id}`}>
+              <li
+                key={item.id}
+                className="flex justify-between items-center bg-white my-2 px-4 rounded-lg shadow-md"
+              >
+                <Link to={`/product/${slug}`}>
                   <div className="flex items-center gap-4">
                     <div>
                       <img
