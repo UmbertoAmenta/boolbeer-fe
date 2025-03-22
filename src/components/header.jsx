3bac438c05c31266e15c106ffc04ce7cf75766c9
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-03-22 16:58:02 +0100

    hotfix: header icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -24,33 +24,38 @@ export default function Header() {
           <nav
             className={`transition-all duration-300 absolute md:relative top-25 md:top-auto w-full right-0 md:w-auto bg-orange-200 md:bg-transparent p-4 md:p-0 flex flex-col md:flex-row md:gap-6 items-center ${
               menuOpen ? "block" : "hidden"
-            } md:flex`}>
+            } md:flex`}
+          >
             <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 items-center">
               <li>
                 <NavLink
                   to="/"
-                  className="hover:bg-white/40 hover:font-bold px-4 py-2 rounded-2xl transition duration-200">
+                  className="hover:bg-white/40 hover:font-bold px-4 py-2 rounded-2xl transition duration-200"
+                >
                   Home
                 </NavLink>
               </li>
               <li>
                 <NavLink
                   to="/search"
-                  className="hover:bg-white/40 hover:font-bold p-2 rounded-2xl transition duration-200">
+                  className="hover:bg-white/40 hover:font-bold p-2 rounded-2xl transition duration-200"
+                >
                   Prodotti
                 </NavLink>
               </li>
               <li>
                 <NavLink
                   to="/about"
-                  className="hover:bg-white/40 hover:font-bold p-2 rounded-2xl transition duration-200">
+                  className="hover:bg-white/40 hover:font-bold p-2 rounded-2xl transition duration-200"
+                >
                   Chi Siamo
                 </NavLink>
               </li>
               <li>
                 <NavLink
                   to="/contact"
-                  className="hover:bg-white/40 hover:font-bold p-2 rounded-2xl transition duration-200">
+                  className="hover:bg-white/40 hover:font-bold p-2 rounded-2xl transition duration-200"
+                >
                   Contatti
                 </NavLink>
               </li>
@@ -62,8 +67,9 @@ export default function Header() {
           {/* Icona Carrello */}
           <Link
             to="/cart"
-            className="relative hover:text-gray-900 hover:font-bold">
-            <i className="fa-solid fa-cart-shopping hover:text-black transition duration-200 text-3xl mr-2"></i>
+            className="relative hover:text-gray-900 hover:font-bold"
+          >
+            <i className="fa-solid fa-cart-shopping hover:text-black transition duration-200 text-3xl"></i>
             {totalQuantity > 0 && (
               <span className="shadow absolute -top-2 -right-1 bg-red-600 text-white text-xs rounded-xl px-2 flex items-center justify-center">
                 {totalQuantity}
@@ -73,8 +79,9 @@ export default function Header() {
 
           <Link
             to="/wishlist"
-            className=" relative hover:text-gray-900 hover:font-bold">
-            <i className="fa-solid fa-heart hover:text-black transition duration-200 text-3xl mr-2"></i>
+            className=" relative hover:text-gray-900 hover:font-bold"
+          >
+            <i className="fa-solid fa-heart hover:text-black transition duration-200 text-3xl"></i>
             {totalItemWishlist > 0 && (
               <span className="shadow absolute -top-2 -right-1 bg-red-600 text-white text-xs rounded-xl px-2 flex items-center justify-center">
                 {totalItemWishlist}
@@ -85,7 +92,8 @@ export default function Header() {
           {/* Hamburger Button */}
           <button
             className="md:hidden text-2xl"
-            onClick={() => setMenuOpen(!menuOpen)}>
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
             <i className="fa-solid fa-bars hover:text-black transition duration-200 text-3xl cursor-pointer"></i>
           </button>
         </div>
